8096acd9879273b4c1c4e3fb419c05923914232c
JC <jcsmileyjr@gmail.com>
2024-05-24 15:47:42 -0500

    Feature: Header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+    return (
+        <header className="w-full text-center py-4">
+            <h1 className="uppercase">Buy Low 📉 Sell High 📈</h1>
+        </header>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Header from './components/header/header'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Test
+    <main className="flex min-h-screen flex-col">
+      <Header />
     </main>
   )
 }
