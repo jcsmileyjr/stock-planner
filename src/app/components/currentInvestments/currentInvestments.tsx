85de4363c8b13872930f0658d9ea71146d2e9665
JC <jcsmileyjr@gmail.com>
2024-05-24 21:40:33 -0500

    Style - Current Investment: UI placement

--- src/app/components/currentInvestments/currentInvestments.tsx
+++ src/app/components/currentInvestments/currentInvestments.tsx
@@ -1,9 +1,22 @@
-
+import StockToggle from "../stockToggle/stockToggle"
 
 export default function CurrentInvestments() {
     return (
-        <section className="col-span-2">
-            <h1>Current Investments</h1>
+        <section className="col-span-2 md:px-4 pb-4 mb-4 border-solid border-b-2 border-slate-500">
+            <h1 className="underline font-bold uppercase text-2xl mb-2">Current Investments</h1>
+            <div className="flex flex-row mb-2">
+                <p className="w-1/5">Name</p>
+                <p className="w-1/3">Current Price</p>
+                <p className="w-1/4">Sell Price</p>
+                <p className="hidden md:block">Purchased Price</p>
+                <p className="hidden md:block">Profit Margin</p>
+                <p className="hidden md:block">Quantity</p>
+                <p className="w-1/4">Flag</p>
+            </div>
+            <div>
+                <StockToggle />
+            </div>
+        
         </section>
     )
 }
---
+++ src/app/components/stockToggle/stockToggle.tsx
@@ -0,0 +1,50 @@
+"use client"
+import {useState} from 'react';
+import Data from '../../data/testData.json';
+import Stock from '../../types/stock';
+import GenericStock from '../../data/genericStock.json';
+
+const refineCurrentInvestment = (data: Stock) => {
+    let refinedData: {[key: string]: string|number} = {}
+    for (const property in data) {
+        if(property === 'purchasedPrice' || property === 'quantity' || property === 'profitMargin' || property === 'targetSellPrice' || property === 'currentPrice' || property === 'symbol' || property === 'flag') {
+            refinedData[property] = data[property];
+        } 
+    }
+    console.log(refinedData)
+    return refinedData;
+}
+
+export default function StockToggle() {
+    const [openStock, setOpenStock] = useState(false);
+    const testStock = refineCurrentInvestment(Data.stocks[0]);
+    
+    return (
+        <div className="flex flex-row">
+            <details className='w-full' open={openStock} onToggle={() => setOpenStock(!openStock)}>
+                <summary className='flex flex-row'>
+                    {Object.keys(testStock).map((key) => {
+                        if(key === 'purchasedPrice' || key === 'quantity' || key === 'profitMargin') {
+                            return (
+                                <p key={`${testStock['symbol']}-${key}`} className="hidden md:block">{testStock[key]}</p>
+                            )
+                        } else if (key === 'symbol') {
+                            return (
+                                <p key={`${testStock['symbol']}-${key}`} className="w-1/5">{testStock[key]}</p>
+                            )
+                        } else if (key === 'currentPrice') {
+                            return (
+                                <p key={`${testStock['symbol']}-${key}`} className="w-1/3">{testStock[key]}</p>
+                            )
+                        } else {
+                            return (
+                                <p key={`${testStock['symbol']}-${key}`} className='w-1/4'>{testStock[key]}</p>
+                            )
+                        }
+                    })}
+                </summary>
+                More tests
+            </details>
+        </div>
+    )
+}
