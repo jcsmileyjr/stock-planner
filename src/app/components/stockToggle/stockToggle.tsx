ee4ded9452728d898816378b8ea7f896b32cdfc0
JC <jcsmileyjr@gmail.com>
2024-05-25 17:32:23 -0500

    Refactor - Current Investment: Calculate stock profit margin

--- src/app/components/stockToggle/stockToggle.tsx
+++ src/app/components/stockToggle/stockToggle.tsx
@@ -13,13 +13,17 @@ const refineCurrentInvestment = (data: stockType) => {
     return refinedData;
 }
 
+const calculatePurchaseStockProfitMargin = (stock: stockType) => {
+    return (stock.currentPrice - stock.purchasedPrice) * stock.quantity;
+}
+
 export default function StockToggle({stock}: {stock: stockType}) {
     const [openStock, setOpenStock] = useState(false);
     // const stock = refineCurrentInvestment(Data.stocks[0]);
     
     return (
         <div className="flex flex-row">
-            <details className='w-full' open={openStock} onToggle={() => setOpenStock(!openStock)}>
+            <details className='w-full my-2' open={openStock} onToggle={() => setOpenStock(!openStock)}>
                 <summary className='flex flex-row'>
                     {Object.keys(stock).map((key) => {
                         if(key === 'purchasedPrice' || key === 'quantity' || key === 'profitMargin') {
@@ -47,7 +51,8 @@ export default function StockToggle({stock}: {stock: stockType}) {
                             return (
                                 <div className='flex flex-row' key={`${stock['symbol']}-${key}`}>
                                     <p className="flex lg:hidden font-medium indent-4 flex-1">{key === 'profitMargin' ? 'Profit Margin' : key==='quantity' ? 'Quantity' : 'Purchased Price'}:</p>
-                                    <p className="flex lg:hidden lg:font-normal flex-1">{key === 'quantity' ? "#":"$"}{stock[key]}</p>
+                                    { key === 'profitMargin' && <p className="flex lg:hidden font-normal flex-1">${calculatePurchaseStockProfitMargin(stock)}</p> }
+                                    { key !== 'profitMargin' && <p className="flex lg:hidden font-normal flex-1">{key === 'quantity' ? "#":"$"}{stock[key]}</p> }
                                 </div>
                             )
                         }
