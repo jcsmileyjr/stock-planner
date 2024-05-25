0e7e2a84db7330d22977e2a1041b91f6799f9d54
JC <jcsmileyjr@gmail.com>
2024-05-25 10:53:56 -0500

    Refactor - Executive Summary: Add data
    Use the static data to provide information to the component.

--- src/app/components/executiveSummary/executiveSummary.tsx
+++ src/app/components/executiveSummary/executiveSummary.tsx
@@ -1,25 +1,60 @@
-import Image from 'next/image'
-import Info from '../../images/info.png'
+import Image from 'next/image';
+import Info from '../../images/info.png';
+import data from '../../types/data';
 
-export default function ExecutiveSummary() {
+
+/**
+ * Determines the number of winners based on the given data.
+ *
+ * @param {data} data - The data object containing stock information.
+ * @return {number} The count of winners.
+ */
+const determineWinners = (data: data) => {
+    let count = 0;
+    data.stocks.forEach((stock) => {
+        if(stock.profitMargin > 0 && stock.status === 'sold') {
+            count++;
+        }
+    })
+    return count;
+}
+
+
+/**
+ * Determines the number of losers based on the given data.
+ *
+ * @param {data} data - The data object containing stock information.
+ * @return {number} The count of losers.
+ */
+const determineLosers = (data: data) => {
+    let count = 0;
+    data.stocks.forEach((stock) => {
+        if(stock.profitMargin < 0 && stock.status === 'sold') {
+            count++;
+        }
+    })
+    return count;
+}
+
+export default function ExecutiveSummary({content}: {content: data}) {
     return (
         <section className=" col-span-1 md:px-4 pb-4 mb-4 border-solid border-b-2 border-slate-500">
             <h1 className="underline font-bold uppercase text-2xl mb-2">Executive Summary</h1>
             <div className="flex flex-row justify-between mb-2">
                 <p className='flex flex-row'>Total Investments  <Image src={Info} alt="info" width={20} height={50} className='h-4 mx-2 self-center' />   :</p>
-                <p>$2500 <span className='text-nowrap'>&#40; Initial: $1750 &#41;</span></p>
+                <p>${content.totalInvestment} <span className='text-nowrap'>&#40; Initial: ${content.initialInvestment} &#41;</span></p>
             </div>
             <div className="flex flex-row justify-between mb-2">
                 <p className='flex flex-row'>Profits (made/loss) <Image src={Info} alt="info" width={20} height={50} className='h-4 mx-2 self-center' />   :</p>
-                <p>$750</p>
+                <p>${content.totalInvestment - content.initialInvestment}</p>
             </div>  
             <div className="flex flex-row justify-between mb-2">
                 <p className='flex flex-row'>Winning Stocks  <Image src={Info} alt="info" width={20} height={50} className='h-4 mx-2 self-center' />   :</p>
-                <p>#4</p>
+                <p>#{determineWinners(content)}</p>
             </div>  
             <div className="flex flex-row justify-between mb-2">
                 <p className='flex flex-row'>Losing Stocks  <Image src={Info} alt="info" width={20} height={50} className='h-4 mx-2 self-center' />   :</p>
-                <p>#2</p>
+                <p>#{determineLosers(content)}</p>
             </div>                                
         
         </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import Image from 'next/image'
 import Header from './components/header/header';
 import ExecutiveSummary from './components/executiveSummary/executiveSummary';
 import CurrentInvestments from './components/currentInvestments/currentInvestments';
 import ScoutInvestments from './components/scoutInvestments/scoutInvestments';
+import TestData from './data/testData.json';
+import Stock from './types/stock';
+
+// TODO: function that refine the data for Current Investments
+
+// TODO: function that refine the data for Scout Investments
 
 export default function Home() {
+  const data = TestData;
   return (
     <main className="flex min-h-screen flex-col">
       <Header />
       <div className='grid md:grid-cols-3 md:grid-rows-2 px-4'>
-        <ExecutiveSummary />
+        <ExecutiveSummary content={data} />
         <CurrentInvestments />
         <ScoutInvestments />
       </div>
