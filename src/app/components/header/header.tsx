2842159ab258d66ca831a5c913f25b5876a7ef07
JC <jcsmileyjr@gmail.com>
2024-05-26 10:46:51 -0500

    Style - Header whitespace

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -2,7 +2,7 @@
 export default function Header() {
     return (
         <header className="w-full text-center py-4">
-            <h1 className="uppercase text-xl lg:text-3xl">Buy Low 📉 Sell High 📈</h1>
+            <h1 className="uppercase text-xl lg:text-3xl lg:mb-4">Buy Low 📉 Sell High 📈</h1>
         </header>
     )
 }
