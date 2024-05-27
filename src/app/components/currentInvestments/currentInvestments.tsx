e3226a1a225f3807ffbe19237a6aeac2a2ed648f
JC <jcsmileyjr@gmail.com>
2024-05-27 16:07:37 -0500

    Style: Update responsive large screen breakpoint to 1130px

--- src/app/components/currentInvestments/currentInvestments.tsx
+++ src/app/components/currentInvestments/currentInvestments.tsx
@@ -9,9 +9,9 @@ export default function CurrentInvestments({content}: {content: stockType[]}) {
                 <p className="flex-1 font-bold">Symbol</p>
                 <p className="flex-1 font-bold">Live Price</p>
                 <p className="flex-1 font-bold">Sell Price</p>
-                <p className="hidden lg:flex flex-1 font-bold">Invest Price</p>
-                <p className="hidden lg:flex flex-1 font-bold">Profit Margin</p>
-                <p className="hidden lg:flex flex-1 font-bold">Quantity</p>
+                <p className="hidden laptop:flex flex-1 font-bold">Invest Price</p>
+                <p className="hidden laptop:flex flex-1 font-bold">Profit Margin</p>
+                <p className="hidden laptop:flex flex-1 font-bold">Quantity</p>
                 <p className="flex-1 font-bold">Flag</p>
             </div>
             <div className="divide-y">
