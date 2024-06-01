98cc80af6dc5130cd823638cc27e5621294da4b9
JC <jcsmileyjr@gmail.com>
2024-06-01 05:58:07 -0500

    Style - Purchase Investment: Update whitespace in page menu & title

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <header className="w-full text-center py-4 flex flex-col laptop:flex-row">
-            <div className='flex items-center justify-end self-center flex-2'>
+            <div className='flex items-center justify-end self-center flex-2 gap-8 md:gap-20'>
                 <h1 className="uppercase text-xl laptop:text-3xl laptop:mb-4">Buy Low 📉 Sell High 📈</h1>
                 <button onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="laptop:hidden">
                     <Image src={Hamburger} className='h-4' alt="menu" width={20} height={50} />
