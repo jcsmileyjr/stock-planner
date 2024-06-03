828d64719088e68310d7a8c1fa48781bb18d86c1
JC <jcsmileyjr@gmail.com>
2024-06-03 11:03:00 -0500

    Feature - Footer: Add financial disclaimer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,8 +1,9 @@
 
 export default function Footer () {
     return (
-        <footer className="fixed bottom-0 left-0 right-0 text-center mb-4 ">
-            <h1 className="text-xl lg:text-3xl lg:mb-4">Data provided by <a className="underline" href="https://intelligence.financialmodelingprep.com/">Intelligence Financial Modeling Prep</a></h1>
+        <footer className=" md:fixed bottom-0 left-0 right-0 text-center mt-12 mb-4 mx-4 ">
+            <p className="text-base">Data provided by <a className="underline" href="https://intelligence.financialmodelingprep.com/">Intelligence Financial Modeling Prep</a></p>
+            <p className="text-base">Disclaimer: Any information provided here is not financial advice and for my personal use only. Always consult with a qualified financial professional before making any investment decisions.</p>
         </footer>
     );
 }
