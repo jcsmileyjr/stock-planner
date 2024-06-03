9e6afbc67d5afa12d17f24974cdf6890e76ae424
JC <jcsmileyjr@gmail.com>
2024-06-03 14:15:56 -0500

    Refactor layout to ensure footer isn't overshadowing content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,12 +100,12 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Header />
-      <div className='grid grid-cols-1 laptop:grid-cols-3 laptop:grid-rows-2 laptop:gap-x-4 px-2 md:px-48 laptop:px-4'>
+      <div className='grid grid-cols-1 laptop:grid-cols-3 laptop:grid-rows-3 laptop:gap-x-4 px-2 md:px-48 laptop:px-4'>
         <ExecutiveSummary content={data} />
         <CurrentInvestments content={currentStocks} />
         <ScoutInvestments content={scoutStocks} />
+        <Footer />
       </div>
-      <Footer />
     </main>
   )
 }
