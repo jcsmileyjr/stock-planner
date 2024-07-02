16b3fc5d4deb9fb69fad6fb3d2781775416b03c8
JC <jcsmileyjr@gmail.com>
2024-07-01 20:43:18 -0500

    Feature (SaleForm): Initial UI for the sale of an investment

--- src/app/saleInvestments/page.tsx
+++ src/app/saleInvestments/page.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/header/header";
 import EditForm from "../components/editForm/editForm";
+import SaleForm from "../components/saleForm/saleForm";
 import getData from "../utils/getData";
 
 export default async function SaleInvestments () {
@@ -11,7 +12,7 @@ export default async function SaleInvestments () {
         <main className="flex min-h-screen flex-col mx-4">
             <Header />
             <h2 className="uppercase text-xl laptop:text-2xl font-bold underline text-center mb-4">Edit Investment Setup</h2>
-            <EditForm content={content} />
+            <SaleForm content={content} />
         </main>
     )
 }
