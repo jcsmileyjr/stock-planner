f0a2ba713ea795339ef159a0a8eedbd530b59bd2
JC <jcsmileyjr@gmail.com>
2024-07-03 08:28:13 -0500

    Refactor (Sale Investment): Mark the investment as sold
    Update the database that an investment is sold.

--- src/app/utils/saleData.ts
+++ src/app/utils/saleData.ts
@@ -4,17 +4,19 @@ import connectDb from '../config/db';
 import Investment from '../models/Investment';
 import StockType from '../types/stockType';
 
+/**
+ * Updates the stock data (as sold) in the database with the provided data.
+ *
+ * @param {StockType} data - The stock data to be updated.
+ * @return {Promise<void>} A promise that resolves after updating the database.
+ */
 const saleData = async (data : StockType) => {
     try {
         await connectDb(); // connect to database
         const serverData = await Investment.find({}); // get data from database
         const content = serverData[0]; // Extract data from server's array        
-        
         const index = content.stocks.findIndex((stock: StockType) => stock.symbol === data.symbol);        
-        const stock_id = content.stocks[index]._id;        
         content.stocks[index] = data;
-        content.stocks[index]._id = stock_id;
-
 
         const investments = new Investment(content); // Create new investment with updated investment information        
         await investments.save(); // Save updated investment
