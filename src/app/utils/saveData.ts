adafd8f048bf26068203b4eaea8be64d792ee05e
JC <jcsmileyjr@gmail.com>
2024-08-02 21:09:28 -0500

    Refactor (Days Invested): Ensure proper dates when creating investments

--- src/app/utils/saveData.ts
+++ src/app/utils/saveData.ts
@@ -3,12 +3,20 @@ import { revalidatePath } from 'next/cache';
 import connectDb from '../config/db';
 import Investment from '../models/Investment';
 import StockType from '../types/stockType';
+import dayjs from 'dayjs';
 
 const saveData = async (data : StockType) => {
     try {
         await connectDb(); // connect to database
         const serverData = await Investment.find({}); // get data from database
         const content = serverData[0]; // Extract data from server's array
+        
+        if (data.status === "purchased") {
+            data.purchaseDate = dayjs().format("MM/DD/YYYY"); // set purchase date to today's date
+        } else {
+            data.purchaseDate = "1/1/2024"; // set purchase date to default if scouted investment
+        }
+        data.saleDate = "1/1/2024"; // set sale date
         content.stocks.push(data); // Add new stock to array
 
         const investments = new Investment(content); // Create new investment with updated investment information
