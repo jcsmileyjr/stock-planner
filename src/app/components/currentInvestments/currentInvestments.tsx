c0726d52ae2f8d9020216a7fa129e52ffc7e7cf5
JC <jcsmileyjr@gmail.com>
2024-08-03 11:56:58 -0500

    Style (dashboard): Whitespace between sections

--- src/app/components/currentInvestments/currentInvestments.tsx
+++ src/app/components/currentInvestments/currentInvestments.tsx
@@ -4,7 +4,7 @@ import stockType from '../../types/stockType';
 export default function CurrentInvestments({content}: {content: stockType[]}) {
     return (
         <section className="col-span-1 laptop:col-span-3 pb-4 mb-4 border-solid border-t-2 border-slate-500">
-            <h1 className="underline font-bold uppercase text-2xl mb-2 pt-4">Current Investments</h1>
+            <h1 className="underline font-bold uppercase text-2xl mb-2 pt-8">Current Investments</h1>
             <div className="flex flex-row mb-2">
                 <p className="flex-1 font-bold">Symbol</p>
                 <p className="flex-1 font-bold">Live Price</p>
