ac3733aad4f6a023d10d44b69ab62781ca51b3a5
JC <jcsmileyjr@gmail.com>
2024-08-25 12:10:17 -0500

    Feature: Unlock middleware password logic

--- src/app/utils/submitInvestments.ts
+++ src/app/utils/submitInvestments.ts
@@ -3,7 +3,7 @@ import saveData from "./saveData";
 import saleData from "./saleData";
 import convertData from "./convertData";
 const submitInvestment = async (type: string, pwd: string, state: any, dispatch: any, router: any, cleanUp: Function) => {
-    if (pwd === process.env.NEXT_PUBLIC_PASSWORD) { 
+    // if (pwd === process.env.NEXT_PUBLIC_PASSWORD) { 
         let data = state.investment;
         if ( type === 'purchaseInvestment') {
             await saveData(data);
@@ -20,11 +20,11 @@ const submitInvestment = async (type: string, pwd: string, state: any, dispatch:
         dispatch({ type: type, content: data });
         router.push('/')
         cleanUp(false); // Clean up state like the password
-    } else {
-        // Throw error
-        console.log("Wrong password")
-        cleanUp(true); // Clean up state like the password
-    }
+    // } else {
+    //     // Throw error
+    //     console.log("Wrong password")
+    //     cleanUp(true); // Clean up state like the password
+    // }
 }
 
 export default submitInvestment;
